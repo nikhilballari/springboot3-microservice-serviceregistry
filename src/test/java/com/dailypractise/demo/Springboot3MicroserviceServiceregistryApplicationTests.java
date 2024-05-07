737acfeaebaf11afa33e257927924d195a4f04c9
nikhilballari <nikhil.ballari@gmail.com>
2024-05-07 03:15:49 +0100

    This is springboot project developed for implementation of microservices architecture using spring boot 3, java 17 and mysql as backend db

---
+++ src/test/java/com/dailypractise/demo/Springboot3MicroserviceServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailypractise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MicroserviceServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
